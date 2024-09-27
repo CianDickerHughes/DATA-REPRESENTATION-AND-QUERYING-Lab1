61ca5acf624291e0eeb1858155e9edaa6dfee52a
CianDickerHughes <g00415413@atu.ie>
2024-09-27 13:46:08 +0000

    2c complete

---
+++ 2c.js
@@ -0,0 +1,10 @@
+// cian dicker-hughes
+// g0041513@atu.ie
+
+// arrow function returns a two nubmers and returns the sum
+let c = (value1, value2) =>{
+    let value = value1 + value2;
+    console.log(value);
+}
+
+c(25, 54);
