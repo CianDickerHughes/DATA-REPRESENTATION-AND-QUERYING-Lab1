e9fd5e97443bfdda60ff69ad04fa09132c7b2901
CianDickerHughes <g00415413@atu.ie>
2024-09-27 14:15:16 +0000

    3b complete

--- 3.js
+++ 3.js
@@ -1,6 +1,20 @@
 // cian dicker-hughes
 // g0041513@atu.ie
 
+// 3.a
 // arrar for strings
 let TaskList = [];
+
+// 3.b
+let addTask = (task)=>{
+    TaskList.push(task);
+    console.log(task +" + has been added to my TaskList ");
+    return TaskList.length;
+}
+
+addTask("Work");
+
+
+
+
 
